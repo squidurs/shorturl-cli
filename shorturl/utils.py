99ae83ef03a7d40b0d3ecf3b8f9a63da9aac8e73
Sydney McClements <smccl96@gmail.com>
2024-11-22 19:49:29 -0800

    Fix message format

--- shorturl/utils.py
+++ shorturl/utils.py
@@ -1,16 +1,21 @@
 import click
+import json
 
 def check_response_status(response, message=None):
+    try:
+        error_message = json.loads(response.text).get("detail", response.text)
+    except json.JSONDecodeError:
+        error_message = response.text
     if response.status_code == 200:
         if message:
             click.echo(message)
         return True
     elif 400 <= response.status_code < 500:
-        click.echo(f"Client error: {response.status_code} - {response.text}")
+        click.echo(f"Client error: {response.status_code} - {error_message}")
     elif 500 <= response.status_code < 600:
-        click.echo(f"Server error: {response.status_code} - {response.text}")
+        click.echo(f"Server error: {response.status_code} - {error_message}")
     else:
-        click.echo(f"Unexpected error: {response.status_code} - {response.text}")
+        click.echo(f"Unexpected error: {response.status_code} - {error_message}")
     return False
 
 def get_auth_token():
